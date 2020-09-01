357b0b0e0b60b0764b76171b3d79c81c96ca6026
Luis Barata da Fonseca Magalhaes/IT/CSUK <Luis.Magalhaes@creditsafe.com>
2020-09-01 13:48:42 +0100

    Added: redirect route, clear profile and initial dashboard functionalities

--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -8,8 +8,14 @@ export function Navbar({ auth: { isAuthenticated, loading }, logout }) {
   const authLinks = (
     <ul>
       <li>
+        <Link to="/dashboard">
+          <i className="fas fa-user" />{" "}
+          <span className="hide-sm">Dashboard</span>
+        </Link>
+      </li>
+      <li>
         <a onClick={logout} href="#!">
-          <i className="i.fas.fa-signout-alt"></i>{" "}
+          <i className="fas fa-sign-out-alt" />{" "}
           <span className="hide-sm">Logout</span>
         </a>
       </li>
